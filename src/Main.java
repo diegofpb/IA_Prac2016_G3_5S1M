e283515d704ef08cfb67615e8c78fb5d9ee2dec1
Roberto Garrido <garrihendrix@gmail.com>
2016-12-09 12:55:52 +0100

    Tester updated

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import algo.AStar;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import models.Station;
+import models.Transition;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,6 +10,8 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -22,9 +25,11 @@ public class Main {
         Type type = new TypeToken<Map<String, Station>>(){}.getType();
         Map<String, Station> stations = gson.fromJson(new InputStreamReader(new FileInputStream(file)), type);
 
-        AStar a = new AStar(stations, "Talleres", "San Bernabé");
+        AStar a = new AStar(stations, "San Bernabé", "Lerdo de Tejada");
         a.solve();
+        List<Transition> result = a.getTransitions();
 
+        System.out.print(true);
     }
 
 }
